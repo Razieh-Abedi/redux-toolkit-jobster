521a27469e77117b3c66bce63bbe7f5e082bca9a
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-03 15:48:38 +0800

    did basic setup of register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,56 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import {Logo} from "../components";
+//import redux toolkit
+//import useNavigator
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 function Register() {
+  const [userInfo, setUserInfo] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <div>Register
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <h1>test</h1>
-    </div>
-  )
+    <Wrapper className="full-page">
+      <form onSubmit={handleSubmit} className="form">
+        <Logo />
+        <h3>login</h3>
+        {/* name field */}
+        <div className="form-row">
+          <label className="form-label" htmlFor="name">
+            name
+          </label>
+          <input
+            className="form-input"
+            name="name"
+            type="text"
+            value={userInfo.name}
+            onChange={handleChange}
+          />
+        </div>
+        {/* email */}
+        <div className="form-row">
+          <label className="form-label" htmlFor="email">
+            email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={userInfo.email}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        {/* password */}
+        <div className="form-row">
+          <label className="form-label" htmlFor="password">
+            password
+          </label>
+          <input
+            type="password"
+            value={userInfo.password}
+            onChange={handleChange}
+            name="password"
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 }
 
-export default Register
+export default Register;
