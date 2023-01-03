5e5183ca6c52c9dbe92cd3c6da61d44cb7e1b6a7
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-03 14:34:42 +0800

    setup error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link } from "react-router-dom";
+import image from "../assets/images/not-found.svg";
 
 function Error() {
   return (
-    <Wrapper>Error</Wrapper>
-  )
+    <Wrapper className="full-page">
+      <div>
+        <img src={image} alt="error" />
+        <h3>Ohh! Page Not Found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">Back to home</Link>
+      </div>
+    </Wrapper>
+  );
 }
 
-export default Error
+export default Error;
