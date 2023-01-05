fe670bb002897bd4b6fc31aecdf738fe6862ef95
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-05 14:28:36 +0800

    basic setup for nested routes in dashboard

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/Navbar";
+
+function Navbar() {
+  return (
+    <Wrapper>
+      {" "}
+      <div>Navbar</div>
+    </Wrapper>
+  );
+}
+
+export default Navbar;
