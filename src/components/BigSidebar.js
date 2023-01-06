da8b812558804008caaa446cd4375c9d2707be02
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-06 14:26:37 +0800

    set up big sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
+import { useSelector } from "react-redux";
 
 function BigSidebar() {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
   return (
     <Wrapper>
-      {" "}
-      <div>BigSidebar</div>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 }
