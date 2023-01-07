95f6df7faeb0eba99eb0346beeb7eac84fa7ade0
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-07 14:01:23 +0800

    set logout functionality for unauthorized users

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -48,7 +48,10 @@ export const updateUser = createAsyncThunk(
       });
       return resp.data;
     } catch (error) {
-      console.log(error.response);
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue("Unauthorized user! Logging out...");
+      }
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
@@ -64,6 +67,7 @@ const userSlice = createSlice({
     logoutUser: (state) => {
       state.user = null;
       state.isSidebarOpen = false;
+      //toast.success("Logout successful!");
       removeUserFromLocalStorage();
     },
   },
