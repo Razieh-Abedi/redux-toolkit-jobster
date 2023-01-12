954a5de89567e7450172184261375cdc82dddbf1
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-12 15:52:55 +0800

    setup the form in search form off all jobs

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/SearchContainer";
+import { useDispatch, useSelector } from "react-redux";
 
 function SearchContainer() {
+  const dispatch = useDispatch();
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+
+  const handleSearch = (e) => {
+    console.log("searching");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>SearchContainer</div>
-  )
+    <Wrapper>
+      <form className="form">
+        <h4>Search form</h4>
+        <div className="form-center">
+          {/* search by position */}
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+          {/* search by status */}
+          <FormRowSelect
+            labelText="Status"
+            name="searchStatus"
+            value={searchStatus}
+            list={["all", ...statusOptions]}
+            handleChange={handleSearch}
+          />
+          {/* search by type */}
+          <FormRowSelect
+            labelText="Type"
+            name="searchType"
+            value={searchType}
+            list={["all", ...jobTypeOptions]}
+            handleChange={handleSearch}
+          />
+          {/* sort */}
+          <FormRowSelect
+            labelText="Sort"
+            name="sort"
+            value={sort}
+            list={[...sortOptions]}
+            handleChange={handleSearch}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            onClick={handleSubmit}
+            disabled={isLoading}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
 
-export default SearchContainer
+export default SearchContainer;
